3098a3426aededcfaea177f172e68a45711fc1b1
cohitre <carlosrr@gmail.com>
2010-03-29 09:36:54 -0700

    Switching to using the syntax gem for syntax highlighting.

--- toopaste.rb
+++ toopaste.rb
@@ -4,10 +4,10 @@ require 'sinatra'
 require 'dm-core'
 require 'dm-validations'
 require 'dm-timestamps'
-require 'syntaxi'
+require 'syntax/convertors/html'
+
 require 'haml'
 require 'sass'
-
 
 DataMapper.setup(:default, ENV['DATABASE_URL'] || "sqlite3://#{Dir.pwd}/toopaste.sqlite3")
 
@@ -22,24 +22,13 @@ class Snippet
   property :created_at, DateTime
   property :updated_at, DateTime
 
-  # validates_present :body
-  # validates_length :body, :minimum => 1
-
-  Syntaxi.line_number_method = 'floating'
-  Syntaxi.wrap_at_column = 80
-  #Syntaxi.wrap_enabled = false
-
-  def formatted_body
-    replacer = Time.now.strftime('[code-%d]')
-    html = Syntaxi.new("[code lang='ruby']#{self.body.gsub('[/code]',
-    replacer)}[/code]").process
-    "<div class=\"syntax syntax_ruby\">#{html.gsub(replacer, 
-    '[/code]')}</div>"
+  def format_body(language)
+    convertor = Syntax::Convertors::HTML.for_syntax "ruby"
+    "<div class=\"syntax syntax_ruby\">#{convertor.convert(self.body)}</div>"
   end
 end
 
 DataMapper.auto_upgrade!
-#File.open('toopaste.pid', 'w') { |f| f.write(Process.pid) }
 
 get '/application.css' do
   headers 'Content-Type' => 'text/css'
@@ -54,7 +43,7 @@ end
 # create
 post '/' do
   @snippet = Snippet.new(:title => params[:snippet_title],
-                         :body  => params[:snippet_body])
+  :body  => params[:snippet_body])
   if @snippet.save
     redirect "/#{@snippet.id}"
   else
